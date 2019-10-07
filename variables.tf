8b325dc9d1331f8a769680bfed873603e37cb861
Mark Menger <m.menger@f5.com>
2019-10-07 14:06:21 -0700

    prefix and count

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+    description = ""
+    value       = ""
+    type        = "string"
+}
+
+variable "f5_instance_count" {
+    description = ""
+    value       = ""
+    type        = "number"
+}
